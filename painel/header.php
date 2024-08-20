21d58f592d20108cda16fa372ed9546d914f75d2
Leo-Slv <leonardo1692004@gmail.com>
2024-08-19 22:25:51 -0300

    Adição do header.php no painel

---
+++ painel/header.php
@@ -0,0 +1,25 @@
+<?php
+session_start(); 
+error_reporting(E_ALL ^ E_WARNING);
+ini_set('display_errors', 1) ;
+uf(!empty($_SESSION['id'])){
+    require_once '../conect.php';
+}
+else{
+    header("Location: logout.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
