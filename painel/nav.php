f70fdb3eae69ad049d8ec12a40a53c6d82819071
Leo-Slv <leonardo1692004@gmail.com>
2024-08-19 22:25:12 -0300

    Adição do nav.php no painel

---
+++ painel/nav.php
@@ -0,0 +1,10 @@
+<div class="row">
+  <div class="col-sm-12"> 
+    <div class="btn-group">
+          <a href='#' class="btn btn-primary">Caroussel</a>
+          <a href='#' class="btn btn-primary">Pacotes</a>
+          <a href='#' class="btn btn-primary">Serviços</a>
+          <a href='logout.php' class="btn btn-danger">Sair</a>
+    </div>
+  </div>
+</div>
