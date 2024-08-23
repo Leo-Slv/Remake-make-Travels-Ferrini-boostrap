0ff38a4e83e54d2c4034cfd95502a155473b0aa4
Leo-Slv <leonardo1692004@gmail.com>
2024-08-22 23:48:37 -0300

    Criação de um modal com formulário ao clicar no botão +imagem

---
+++ painel/carousel/modal.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="upload" data-backdrop="static">
+    <div class="modal-dialog modal-md div modal-content">
+        <form action="" class="form-group">
+            <div class="modal-header">
+                Upload Imagem
+            </div>
+            <div class="modal-body">
+                <input type="file" name="imagem" class="form-control">
+                <br>
+                <Textarea name="descrição" class="form-control" rows="5" placeholder="descrição"></Textarea>
+                <br>
+                <select name="status" class="form-control">
+                    <option value="1">ativo</option>
+                    <option value="0">inativo</option>
+                </select>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" data-dimiss="modal">
+                    Fechar
+                </button>
+                <input type="submit" class="btn btn-primary" name="action" value="Cadastrar">
+            </div>
+        </form>
+    </div>
+</div>
