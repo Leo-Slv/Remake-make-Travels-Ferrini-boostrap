7a2a8f8f5e9116f458025d134a49a5de4598679b
Leo-Slv <leonardo1692004@gmail.com>
2024-09-04 13:50:32 -0300

    Inserção da Tabela carousel

--- banco.sql
+++ banco.sql
@@ -10,9 +10,17 @@ cd_senha varchar(128) not null,
 dt_registro datetime not null default current_timestamp
 );
 
+create table tb_carousel (
+cd_carousel int auto_increment primary key,
+url_imagem_carousel varchar(100) not null,
+ds_carousel varchar(120),
+ic_active char(1),
+st_carousel char(1) not null
+);
+
 insert into tb_usuario set
 nm_usuario = 'Leonardo',
 nm_email= 'leonardo@mail.com',
 cd_senha = sha2('123',256);
 
-select * from tb_usuario;
+select * from tb_carousel;
