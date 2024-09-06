d041d7480ee7d1293f7f697485422a64bbed6bc3
Leo-Slv <leonardo1692004@gmail.com>
2024-09-06 11:17:37 -0300

    Script jquery para aplicar o click aos botões delete e edit

---
+++ painel/carousel/script.php
@@ -0,0 +1,21 @@
+<script>
+  $(document).on('click', '.edit', function(){
+    var cd = $(this).attr('cd');
+    $('.modal #cd').val(cd);
+    var status = $(this).attr('status');
+    $('.modal #status').val(status);
+    var descricao = $(this).attr('descricao');
+    $('.modal #descricao').val(descricao);
+  });
+
+  $(document).on('click', '.delete', function(){
+    var cd = $(this).attr('cd');
+    $('.modal #cd').val(cd);
+    var status = $(this).attr('status');
+    $('.modal #status').val(status);
+    var descricao = $(this).attr('descricao');
+    $('.modal #descricao').val(descricao);
+  });
+
+
+</script>
