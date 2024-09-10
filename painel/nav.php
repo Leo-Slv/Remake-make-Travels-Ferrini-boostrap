17ae79f674ee3f3b337341e6295e1331937dc32b
Leo-Slv <leonardo1692004@gmail.com>
2024-09-10 10:00:28 -0300

    Atribuição de pagina ao botão pacotes do nav

--- painel/nav.php
+++ painel/nav.php
@@ -2,7 +2,7 @@
   <div class="col-sm-12 text-center mt-3"> 
     <div class="btn-group">
           <a href='carousel.php' class="btn btn-primary">Caroussel</a>
-          <a href='#' class="btn btn-primary">Pacotes</a>
+          <a href='pacotes.php' class="btn btn-primary">Pacotes</a>
           <a href='#' class="btn btn-primary">Serviços</a>
           <a href='logout.php' class="btn btn-danger">Sair</a>
     </div>
