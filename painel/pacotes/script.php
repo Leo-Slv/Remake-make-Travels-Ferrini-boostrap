e9616e0bc4e957c1eb754642912c3506ae890d46
Leo-Slv <leonardo1692004@gmail.com>
2024-09-11 13:37:49 -0300

    Passagem dos parâmetros associados a table pacote

--- painel/pacotes/script.php
+++ painel/pacotes/script.php
@@ -2,10 +2,16 @@
   $(document).on('click', '.edit', function(){
     var cd = $(this).attr('cd');
     $('.modal #cd').val(cd);
+    var destino = $(this).attr('destino');
+    $('.modal #destino').val(destino);
+    var periodo = $(this).attr('periodo');
+    $('.modal #periodo').val(periodo);
+    var acomodacao = $(this).attr('acomodacao');
+    $('.modal #acomodacao').val(acomodacao);
+    var parcela = $(this).attr('parcela');
+    $('.modal #parcela').val(parcela);
     var status = $(this).attr('status');
     $('.modal #status').val(status);
-    var descricao = $(this).attr('descricao');
-    $('.modal #descricao').val(descricao);
   });
 
   $(document).on('click', '.delete', function(){
