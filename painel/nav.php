af437afa7850beec8f031b799a49aab6537fdf9e
Leo-Slv <leonardo1692004@gmail.com>
2024-09-12 19:34:38 -0300

    Alteração do href para o servico.php

--- painel/nav.php
+++ painel/nav.php
@@ -3,7 +3,7 @@
     <div class="btn-group">
           <a href='carousel.php' class="btn btn-primary">Caroussel</a>
           <a href='pacotes.php' class="btn btn-primary">Pacotes</a>
-          <a href='#' class="btn btn-primary">Serviços</a>
+          <a href='servico.php' class="btn btn-primary">Serviços</a>
           <a href='logout.php' class="btn btn-danger">Sair</a>
     </div>
   </div>
