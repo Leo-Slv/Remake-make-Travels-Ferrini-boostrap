b99db3bcd06aafd5cf595e7d6ba10efa6adefb89
Leo-Slv <leonardo1692004@gmail.com>
2024-09-13 17:33:11 -0300

    Funções para chamada de serviços no index

--- function.php
+++ function.php
@@ -17,4 +17,18 @@ function ListarPacotes(){
         return $res;
     }
   }
+
+  function ListarServicos(){
+    $sql = 'select 
+    cd_servico,
+    nm_servico,
+    ds_servico,
+    url_imagem_servico
+    from 
+    tb_servico';
+    $res = $GLOBALS['con']->query($sql);
+    if($res->num_rows > 0){
+      return $res;
+    }
+  }
 ?>
